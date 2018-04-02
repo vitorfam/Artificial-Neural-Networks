4e14ba381a7487ab826e78568483652999cebfd6
Vitor Ferraz <vitorferraz94@hotmail.com>
2018-04-02 16:56:55 -0300

    Trabalhando no y_in

--- js/Perceptron-04/perceptron.js
+++ js/Perceptron-04/perceptron.js
@@ -1,10 +1,10 @@
 function y_in(b, inputs, weights) {
-    let x = inputs.map(function (x, i) {
-        return x * weights[i];
-    });
-    return b + x.reduce(function (vold, vnew, i) {
-        return vold + vnew
-    }, 0);
+    for(var i = 0; i < inputs.length; i++ ){ 
+        inputs[i].map(function (x, index) {
+             return x * w[index] 
+            }).reduce(function (vOld, vNew) { 
+                return vOld + vNew },0); 
+            }
 }
 
 function activationFunc(y_in) {
@@ -22,24 +22,48 @@ function activationFunc(y_in) {
 //Alpha is the learning rate.
 
 function weightUptade(weights, alpha, targets, inputs) {
-    return weights.map((wi, i) => wi + alpha * target[i] * inputs[i]);
+    var w = [];
+    for (var i = 0; i < inputs.length; i++) {
+        let input = inputs[i];
+        w = weights.map(function (wi, x) {
+            return wi + (alpha * target[i] * input[x]);
+        });
+    }
+    console.log(w);
 }
 
 function train(inputs, weights, bias, target, alpha) {
     let yIn = y_in(bias, inputs[0], weights);
     let y = activationFunc(yIn);
+
     if (y == target[0]) {
         console.log(weights);
-
         return weights;
     } else {
-        console.log(target);
-
-        return train(inputs.slice(1), weights, bias, target.slice(1), alpha);
+        console.log(weights);
+        weights = weightUptade(weights, alpha, target, inputs);
+        return train(inputs, weights, bias, target, alpha);
     }
 }
 
 var target = [1, -1];
-var x = [1, -1, -1, -1, 1, -1, 1, -1, 1, -1, -1, -1, 1, -1, -1, -1, 1, -1, 1, -1, 1, -1, -1, -1, 1];
+var s = [1, -1, -1, -1, 1, -1, 1, -1, 1, -1, -1, -1, 1, -1, -1, -1, 1, -1, 1, -1, 1, -1, -1, -1, 1];
 var t = [1, 1, 1, 1, 1, -1, -1, 1, -1, -1, -1, -1, 1, -1, -1, -1, -1, 1, -1, -1, -1, -1, 1, -1, -1];
-var inputs = [x, t];
+var w = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+var inputs = [s, t];
+var alpha = 0.3;
+w = weightUptade(w, 0.5, target, inputs);
+y_in(1, inputs, w);
+// train(inputs, w, 1, target, 0.3);
+
+// var w1 = w.map(function (wi, index) {
+//     return wi + alpha * inputs[index];
+// });
+// console.log(w1);
+
+// var w2 = w.map(function (wi, x) {
+//     console.log(x);
+//     return wi + (alpha * target[1] * s[x]);
+// });
+
+// console.log(w2);
