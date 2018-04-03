8bc0da8f822ca821569cb819891bd8097692c128
vitorfam <vitorferrazamaral@gmail.com>
2018-04-03 20:48:07 -0300

    y_in funcionando

--- js/Perceptron-04/perceptron.js
+++ js/Perceptron-04/perceptron.js
@@ -1,13 +1,13 @@
-function y_in(b, inputs, weights) {
-    return
-    for (var i = 0; i < inputs.length; i++) {
-        b + inputs[i].map(function (x, index) {
-            return x * weights[index]
-        }).reduce(function (vOld, vNew) {
-            return vOld + vNew
-        }, 0);
-    }
+function y_in(a, b, c) {
+    return a + b.map(function (x, index) {
+        return x * c[index]
+    }).reduce(function (vOld, vNew) {
+        return vOld + vNew
+    }, 0);
 }
+
+var net = y_in(0, [1, 1], [0, 0]);
+console.log(net);
 
 function activationFunc(y_in) {
     let theta = 1;
@@ -23,12 +23,12 @@ function activationFunc(y_in) {
 
 //Alpha is the learning rate.
 
-function weightUptade(weights, alpha, targets, inputs) {
+function weightUptade(weights, alpha, target, inputs) {
     var w = [];
     for (var i = 0; i < inputs.length; i++) {
         let input = inputs[i];
         w = weights.map(function (wi, x) {
-            return wi + (alpha * target[i] * input[x]);
+            return wi + (alpha * target * input[x]);
         });
     }
     // console.log(w);
@@ -55,31 +55,3 @@ var t = [1, 1, 1, 1, 1, -1, -1, 1, -1, -1, -1, -1, 1, -1, -1, -1, -1, 1, -1, -1,
 var w = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 var inputs = [s, t];
 var alpha = 0.3;
-
-w = weightUptade(w, 0.5, target, inputs);
-
-// for (var i = 0; i < inputs.length; i++) {
-//     console.log(1 + inputs[i].map(function (x, index) {
-//         return x * w[index]
-//     }).reduce(function (vOld, vNew) {
-//         return vOld + vNew
-//     }, 0));
-// }
-console.log(1 + inputs[1].map(function (x, index) {
-    return x * w[index]
-}).reduce(function (vOld, vNew) {
-    return vOld + vNew
-}, 0));
-// train(inputs, w, 1, target, 0.3);
-
-// var w1 = w.map(function (wi, index) {
-//     return wi + alpha * inputs[index];
-// });
-// console.log(w1);
-
-// var w2 = w.map(function (wi, x) {
-//     console.log(x);
-//     return wi + (alpha * target[1] * s[x]);
-// });
-
-// console.log(w2);
